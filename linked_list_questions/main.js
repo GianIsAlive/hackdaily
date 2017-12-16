62f9510d8f4676595be7871eeaa11dde3bd53f94
GianIsAlive <gianthedesigner@gmail.com>
2017-12-16 14:35:55 -0500

    add remove duplicate node question

--- linked_list_questions/main.js
+++ linked_list_questions/main.js
@@ -1,25 +1,36 @@
-// Create a function that deletes a node of a specific value from Linked List
-
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
-function deleteNode(head, val) {
+// Create a function that deletes a node of a specific value from Linked List
+function deleteNode(linkedList, val) {
+  let head = linkedList;
   if (head.value === val) {
     return head.next;
   }
   while (head.next) {
     if (head.next.value === val) {
-      if (head.next.next !== null) {
-        head.next = head.next.next;
-        return head;
-      } else {
-        head.next = null;
-        return head;
-      }
+      head.next = head.next.next;
     }
     head = head.next;
   }
-  return head;
+  return linkedList;
+}
+
+// Remove duplicate node from a unsorted linked list
+// Bonus: do it without creating a temporary buffer
+function removeDupNode(linkedList) {
+  let head = linkedList;
+  // alternative, one can use hashmap to store values
+  let set = new Set([head.value]);
+  while (head.next) {
+    if (set.has(head.next.value)) {
+      head.next = head.next.next;
+    } else {
+      set.add(head.next.value);
+      head = head.next;
+    }
+  }
+  return linkedList;
 }
